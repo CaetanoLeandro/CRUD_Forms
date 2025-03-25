bb55550659b64006616883bee933d2b03f53ac7f
Leandro Caetano <leandro.caetano@linx.com.br>
2025-03-25 01:08:49 -0300

    Ajustes parciais

--- CRUD_Forms/src/app/app.module.ts
+++ CRUD_Forms/src/app/app.module.ts
@@ -2,11 +2,16 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
-import { routes } from './app.routes';
 import { UserEditComponent } from './user-edit/user-edit.component';
 import { UserListComponent } from './user-list/user-list.component';
+
+export const routes: Routes = [
+  { path: '', component: UserListComponent },
+  { path: 'edit/:id', component: UserEditComponent },
+  { path: 'new', component: UserEditComponent },
+];
 
 @NgModule({
   declarations: [AppComponent, UserEditComponent, UserListComponent],
--- CRUD_Forms/src/app/app.routes.ts
+++ CRUD_Forms/src/app/app.routes.ts
@@ -1,9 +1,9 @@
-import { Routes } from '@angular/router';
-import { UserListComponent } from './user-list/user-list.component';
-import { UserEditComponent } from './user-edit/user-edit.component';
+// import { Routes } from '@angular/router';
+// import { UserListComponent } from './user-list/user-list.component';
+// import { UserEditComponent } from './user-edit/user-edit.component';
 
-export const routes: Routes = [
-  { path: '', component: UserListComponent },
-  { path: 'edit/:id', component: UserEditComponent },
-  { path: 'new', component: UserEditComponent },
-];
+// export const routes: Routes = [
+//   { path: '', component: UserListComponent },
+//   { path: 'edit/:id', component: UserEditComponent },
+//   { path: 'new', component: UserEditComponent },
+// ];
