379d2a1271e75ea48a42a4bfb8a79acb2f38f48c
Leandro Caetano <leandro.caetano@linx.com.br>
2025-03-25 00:00:03 -0300

    user list component

---
+++ CRUD_Forms/src/app/user-list/user-list.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { FormService, User } from '../user.service';
+
+
+@Component({
+  selector: 'app-user-list',
+  templateUrl: './user-list.component.html',
+  styleUrl: './user-list.component.sass',
+})
+export class UserListComponent implements OnInit {
+  users: User[] = [];
+
+  constructor(private formService: FormService) {}
+
+  ngOnInit(): void {
+    this.loadUsers();
+  }
+
+  loadUsers(): void {
+    this.formService.getUsers().subscribe((data) => (this.users = data));
+  }
+
+  deleteUser(id: number): void {
+    this.formService.deleteUser(id).subscribe(() => this.loadUsers());
+  }
+}
